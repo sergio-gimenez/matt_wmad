7818669c0e30512171d67e3c760dbb3c4a12f42e
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-12-11 08:57:08 -0500

    Implemented MessageWall and RemoteLogin (simple)

--- MessageWall_exercise/src/java/demo/impl/MessageWall_and_RemoteLogin_Impl.java
+++ MessageWall_exercise/src/java/demo/impl/MessageWall_and_RemoteLogin_Impl.java
@@ -11,13 +11,13 @@ import java.util.List;
 public class MessageWall_and_RemoteLogin_Impl implements RemoteLogin, MessageWall {
 
     private List<Message> messages;
-    
+
     /*
     Users database is a hashmap where:
         - key: user
         - value: password
-    */
-    private HashMap<String, String> usersdb ;
+     */
+    private HashMap<String, String> usersdb;
 
     public MessageWall_and_RemoteLogin_Impl() {
         // Instantiating here because constructor has no args
@@ -25,11 +25,11 @@ public class MessageWall_and_RemoteLogin_Impl implements RemoteLogin, MessageWal
         usersdb = new HashMap<String, String>();
         usersdb.put("cervantes", "1234");
         usersdb.put("shakespeare", "1234");
-    }        
-    
+    }
+
     @Override
     public UserAccess connect(String usr, String passwd) {
-        if (usersdb.containsKey(usr) && usersdb.containsValue(passwd)){
+        if (usersdb.containsKey(usr) && usersdb.containsValue(passwd)) {
             return (new UserAccess_Impl(this, usr));
         }
         return null;
@@ -42,22 +42,20 @@ public class MessageWall_and_RemoteLogin_Impl implements RemoteLogin, MessageWal
 
     @Override
     public boolean delete(String user, int index) {
-        if(messages.get(index).getOwner().equals(user)){
-            return (messages.remove(index)== null);
+        if (messages.get(index).getOwner().equals(user)) {
+            return (messages.remove(index) == null);
         }
-            return false;
+        return false;
     }
 
     @Override
     public Message getLast() {
-        try{
+        try {
             return messages.get(messages.size() - 1);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("List is empty");
             return null;
         }
-        
     }
 
     @Override
