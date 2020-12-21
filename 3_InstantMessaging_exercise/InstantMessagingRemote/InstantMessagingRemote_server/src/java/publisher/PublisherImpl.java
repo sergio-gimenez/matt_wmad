1c87ac584a4263c326d9681adc426eb5bc9533cc
Sergio Gimenez <sergiogimenezanton@gmail.com>
2020-12-21 13:10:57 -0500

    Impemented Publisher class

--- 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_server/src/java/publisher/PublisherImpl.java
+++ 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_server/src/java/publisher/PublisherImpl.java
@@ -8,50 +8,62 @@ import java.util.List;
 import subscriber.Subscriber;
 import subscriber.SubscriberImpl;
 import javax.websocket.Session;
+import util.Subscription_close.Cause;
 
 public class PublisherImpl implements Publisher {
 
-  private List<Subscriber> subscriberSet;
-  private int numPublishers;
-  private Topic topic;
+    private List<Subscriber> subscriberSet;
+    private int numPublishers;
+    private Topic topic;
 
-  public PublisherImpl(Topic topic) {
-    subscriberSet = new ArrayList<Subscriber>();
-    numPublishers = 1;
-    this.topic = topic;
-  }
+    public PublisherImpl(Topic topic) {
+        subscriberSet = new ArrayList<Subscriber>();
+        numPublishers = 1;
+        this.topic = topic;
+    }
 
-  @Override
-  public void incPublishers() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void incPublishers() {
+        numPublishers++;
+    }
 
-  @Override
-  public int decPublishers() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public int decPublishers() {
+        numPublishers--;
+        return numPublishers;
+    }
 
-  @Override
-  public void attachSubscriber(Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void attachSubscriber(Subscriber subscriber) {
+        // TODO implement trycatch here ?
+        if (!subscriberSet.contains(subscriber)) {
+            this.subscriberSet.add(subscriber);
+        }
+    }
 
-  @Override
-  public boolean detachSubscriber(Subscriber subscriber) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public boolean detachSubscriber(Subscriber subscriber) {
+        subscriber.onClose(new Subscription_close(topic, Cause.SUBSCRIBER));
+        return subscriberSet.remove(subscriber);
+    }
 
-  @Override
-  public void detachAllSubscribers() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void detachAllSubscribers() {
+        for (Subscriber subscriber : subscriberSet) {
+            subscriber.onClose(new Subscription_close(topic, Cause.PUBLISHER));
+        }
+        subscriberSet.removeAll(this.subscriberSet);
+    }
 
-  @Override
-  public void publish(Message message) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-  
-  public Subscriber subscriber(Session session) {
+    @Override
+    public void publish(Message message) {
+        // Post each message from each user subscribed to me (publisher)
+        for (Subscriber subscriber : subscriberSet) {
+            subscriber.onMessage(message);
+        }
+    }
+
+public Subscriber subscriber(Session session) {
     for (Subscriber subscriber : subscriberSet) {
       SubscriberImpl subscriberImpl = (SubscriberImpl) subscriber;
       if (subscriberImpl.session == session) {
