c11347218d2080662164fe65ad89247e14b3b863
martiriera <martiriera13@gmail.com>
2021-01-06 17:58:50 +0100

    Update c_RegistrationActivity.java

--- WhatsApp_exercise/app/src/main/java/edu/upc/whatsapp/c_RegistrationActivity.java
+++ WhatsApp_exercise/app/src/main/java/edu/upc/whatsapp/c_RegistrationActivity.java
@@ -1,6 +1,7 @@
 package edu.upc.whatsapp;
 
 import edu.upc.whatsapp.comms.RPC;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
@@ -20,79 +21,91 @@ import entity.UserInfo;
 
 public class c_RegistrationActivity extends Activity implements View.OnClickListener {
 
-  _GlobalState globalState;
-  ProgressDialog progressDialog;
-  User user;
-  OperationPerformer operationPerformer;
-
-  @Override
-  public void onCreate(Bundle icicle) {
-    super.onCreate(icicle);
-    globalState = (_GlobalState)getApplication();
-    setContentView(R.layout.c_registration);
-    ((Button) findViewById(R.id.editregistrationButton)).setOnClickListener(this);
-  }
-
-  public void onClick(View arg0) {
-    if (arg0 == findViewById(R.id.editregistrationButton)) {
-
-      //...
-
-      progressDialog = ProgressDialog.show(this, "RegistrationActivity", "Registering for service...");
-      // if there's still a running thread doing something, we don't create a new one
-      if (operationPerformer == null) {
-        operationPerformer = new OperationPerformer();
-        operationPerformer.start();
-      }
-    }
-  }
-
-  private class OperationPerformer extends Thread {
+    _GlobalState globalState;
+    ProgressDialog progressDialog;
+    User user;
+    OperationPerformer operationPerformer;
 
     @Override
-    public void run() {
-      Message msg = handler.obtainMessage();
-      Bundle b = new Bundle();
-
-      //...
-
-      msg.setData(b);
-      handler.sendMessage(msg);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        globalState = (_GlobalState) getApplication();
+        setContentView(R.layout.c_registration);
+        ((Button) findViewById(R.id.editregistrationButton)).setOnClickListener(this);
     }
-  }
 
-  Handler handler = new Handler() {
-    @Override
-    public void handleMessage(Message msg) {
+    public void onClick(View arg0) {
+        if (arg0 == findViewById(R.id.editregistrationButton)) {
 
-      operationPerformer = null;
-      progressDialog.dismiss();
+            User user = new User();
+            UserInfo userInfo = new UserInfo();
 
-      UserInfo userInfo = (UserInfo) msg.getData().getSerializable("userInfo");
+            user.setLogin(findViewById(R.id.editregistrationUserText).toString());
+            user.setPassword(findViewById(R.id.editregistrationPasswordText).toString());
+            user.setEmail(findViewById(R.id.editregistrationEmailText).toString());
+            userInfo.setName(findViewById((R.id.editregistrationNameText)).toString());
+            userInfo.setSurname(findViewById((R.id.editregistrationSurnameText)).toString());
 
-      if (userInfo.getId() >= 0) {
-        toastShow("Registration successful");
+            user.setUserInfo(userInfo);
 
-        //...
-
-        finish();
-      }
-      else if (userInfo.getId() == -1) {
-        toastShow("Registration unsuccessful,\nlogin already used by another user");
-      }
-      else if (userInfo.getId() == -2) {
-        toastShow("Not registered, connection problem due to: " + userInfo.getName());
-        System.out.println("--------------------------------------------------");
-        System.out.println("error!!!");
-        System.out.println(userInfo.getName());
-        System.out.println("--------------------------------------------------");
-      }
+            progressDialog = ProgressDialog.show(this, "RegistrationActivity", "Registering for service...");
+            // if there's still a running thread doing something, we don't create a new one
+            if (operationPerformer == null) {
+                operationPerformer = new OperationPerformer();
+                operationPerformer.start();
+            }
+        }
     }
-  };
 
-  private void toastShow(String text) {
-    Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
-    toast.setGravity(0, 0, 200);
-    toast.show();
-  }
+    private class OperationPerformer extends Thread {
+
+        @Override
+        public void run() {
+            Message msg = handler.obtainMessage();
+            Bundle b = new Bundle();
+
+            UserInfo userInfo = RPC.registration(user);
+            b.putSerializable("userInfo", userInfo);
+
+            globalState.my_user = userInfo;
+
+            msg.setData(b);
+            handler.sendMessage(msg);
+        }
+    }
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            operationPerformer = null;
+            progressDialog.dismiss();
+
+            // Get the userInfo from the msg
+            UserInfo userInfo = (UserInfo) msg.getData().getSerializable("userInfo");
+
+            if (userInfo.getId() >= 0) {
+                toastShow("Registration successful");
+                // An Intent object is the communication unit between components of the same or different applications.
+                Intent intent = new Intent(c_RegistrationActivity.this, d_UsersListActivity.class);
+                startActivity(intent);
+
+                // finish();
+            } else if (userInfo.getId() == -1) {
+                toastShow("Registration unsuccessful,\nlogin already used by another user");
+            } else if (userInfo.getId() == -2) {
+                toastShow("Not registered, connection problem due to: " + userInfo.getName());
+                System.out.println("--------------------------------------------------");
+                System.out.println("error!!!");
+                System.out.println(userInfo.getName());
+                System.out.println("--------------------------------------------------");
+            }
+        }
+    };
+
+    private void toastShow(String text) {
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+        toast.setGravity(0, 0, 200);
+        toast.show();
+    }
 }
