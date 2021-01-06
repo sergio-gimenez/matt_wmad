df56dd83a74bb96fc05d6f165f5b0b2d8c496a36
martiriera <martiriera13@gmail.com>
2021-01-06 17:44:56 +0100

    Update b_LoginActivity.java

--- WhatsApp_exercise/app/src/main/java/edu/upc/whatsapp/b_LoginActivity.java
+++ WhatsApp_exercise/app/src/main/java/edu/upc/whatsapp/b_LoginActivity.java
@@ -30,14 +30,18 @@ public class b_LoginActivity extends Activity implements View.OnClickListener {
     super.onCreate(icicle);
     globalState = (_GlobalState)getApplication();
     setContentView(R.layout.b_login);
-    ((Button) findViewById(R.id.editloginButton)).setOnClickListener(this);
+    (findViewById(R.id.editloginButton)).setOnClickListener(this);
   }
 
   public void onClick(View arg0) {
     if (arg0 == findViewById(R.id.editloginButton)) {
 
-        //...
+        // Get the texts from the login xml and set them to the user object
+        User user = new User();
+        user.setLogin(findViewById(R.id.editloginUserText).toString());
+        user.setPassword(findViewById(R.id.editloginPasswordText).toString());
 
+        // With processDialog we indicate a communication in progress
         progressDialog = ProgressDialog.show(this, "LoginActivity", "Logging into the server...");
         // if there's still a running thread doing something, we don't create a new one
         if (operationPerformer == null) {
@@ -54,13 +58,18 @@ public class b_LoginActivity extends Activity implements View.OnClickListener {
       Message msg = handler.obtainMessage();
       Bundle b = new Bundle();
 
-      //...
+      // Log in with the RPC and get the userInfo back
+      UserInfo userInfo = RPC.login(user);
+      // We put the userInfo on both message bundle and globalState
+      b.putSerializable("userInfo", userInfo);
+      globalState.my_user = userInfo;
 
       msg.setData(b);
       handler.sendMessage(msg);
     }
   }
 
+  // Handler to interact with the UI thread
   Handler handler = new Handler() {
     @Override
     public void handleMessage(Message msg) {
@@ -68,14 +77,16 @@ public class b_LoginActivity extends Activity implements View.OnClickListener {
       operationPerformer = null;
       progressDialog.dismiss();
 
+      // Get the userInfo from the msg
       UserInfo userInfo = (UserInfo) msg.getData().getSerializable("userInfo");
 
       if (userInfo.getId() >= 0) {
         toastShow("Login successful");
+        // An Intent object is the communication unit between components of the same or different applications.
+        Intent intent = new Intent(b_LoginActivity.this, d_UsersListActivity.class);
+        startActivity(intent);
 
-        //...
-
-        finish();
+        // finish();
       }
       else if (userInfo.getId() == -1){
         toastShow("Login unsuccessful, try again please.");
