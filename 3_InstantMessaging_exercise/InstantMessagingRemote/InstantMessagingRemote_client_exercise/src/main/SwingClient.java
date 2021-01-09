fb395a7d115e59a36c97d0bbf6588e4f41359d63
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-01-09 07:50:13 -0500

    Imported SwingClient

--- 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_client_exercise/src/main/SwingClient.java
+++ 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_client_exercise/src/main/SwingClient.java
@@ -6,13 +6,18 @@ import util.Topic;
 import subscriber.SubscriberImpl;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import javax.swing.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import publisher.Publisher;
 import subscriber.Subscriber;
 import topicmanager.TopicManager;
 import topicmanager.TopicManagerStub;
+import util.Subscription_check.Result;
+import util.Subscription_close;
+import util.Subscription_close.Cause;
 
 public class SwingClient {
 
@@ -102,88 +107,174 @@ public class SwingClient {
     frame.setVisible(true);
   }
 
-  class showTopicsHandler implements ActionListener {
+    // TODO es pot crear aquest mètode?
+    private void refreshTopicsSubscriptionsBoxes() {
 
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-    }
-  }
+        topic_list_TextArea.setText("");
+        ArrayList<Topic> topics = (ArrayList) topicManager.topics();
 
-  class newPublisherHandler implements ActionListener {
+        for (Topic topic : topics) {
+            topic_list_TextArea.append(topic.name + "\n");
+        }
 
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-    }
-  }
+        my_subscriptions_TextArea.setText("");
 
-  class newSubscriberHandler implements ActionListener {
+        for (Topic topic : my_subscriptions.keySet()) {
+            my_subscriptions_TextArea.append(topic.name + "\n");
+        }
 
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-    }
-  }
-
-  class UnsubscribeHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-    }
-  }
-
-  class postEventHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-    }
-  }
-
-  class CloseAppHandler implements ActionListener {
-
-    public void actionPerformed(ActionEvent e) {
-      
-      //...
-      
-      System.out.println("one user closed");
-      System.exit(0);
-    }
-  }
-
-  class CloseWindowHandler implements WindowListener {
-
-    public void windowDeactivated(WindowEvent e) {
     }
 
-    public void windowActivated(WindowEvent e) {
+    
+    class showTopicsHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            ArrayList<Topic> topics = (ArrayList) topicManager.topics();
+            topic_list_TextArea.setText("");
+            for (Topic topic : topics) {
+                topic_list_TextArea.append(topic.name + "\n");
+            }
+        }
     }
 
-    public void windowIconified(WindowEvent e) {
+    class newPublisherHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String currentTopicName = argument_TextField.getText();
+            ArrayList<Topic> topics = (ArrayList) topicManager.topics();
+            argument_TextField.setText("");
+            boolean topicFound = false;
+
+            if (publisherTopic != null) {
+                messages_TextArea.append("Removed as publisher of topic: " + publisherTopic.name + "\n");
+                topicManager.removePublisherFromTopic(publisherTopic);                
+            }
+
+            for (Topic topic : topics) {
+
+                if (topic.name.equals(currentTopicName)) {                    
+                    topicManager.removePublisherFromTopic(topic);                    
+                    
+                    publisherTopic = topic;
+                    topicFound = true;                    
+                    break;
+                }
+            }
+            if (!topicFound) {
+                // If topic hasn't been found, create new one
+                publisherTopic = new Topic(currentTopicName);
+            }
+            publisher = topicManager.addPublisherToTopic(publisherTopic);
+            publisher_TextArea.setText(publisherTopic.name);
+            messages_TextArea.append("Added as publisher of topic: " + publisherTopic.name + "\n");
+        }
     }
 
-    public void windowDeiconified(WindowEvent e) {
+    class newSubscriberHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String topicToSubscribe = argument_TextField.getText();
+            ArrayList<Topic> topics = (ArrayList) topicManager.topics();
+            argument_TextField.setText("");
+
+            for (Topic topic : topics) {
+                // Check if already subscribed
+                // TODO Implement 
+                if (topic.name.equals(topicToSubscribe) && !my_subscriptions.containsKey(topic)) {
+
+                    Subscriber subscriber = new SubscriberImpl(SwingClient.this);
+                    Subscription_check subscription_check = topicManager.subscribe(topic, subscriber);
+
+                    if (subscription_check.result == Result.OKAY) {
+                        my_subscriptions.put(topic, subscriber);
+                        my_subscriptions_TextArea.append(topic.name + "\n");
+                        messages_TextArea.append("Added as subscriber of topic: " + topic.name + "\n");
+                    }
+                }
+            }
+        }
     }
 
-    public void windowClosed(WindowEvent e) {
+    class UnsubscribeHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String topicToUnsubscribe = argument_TextField.getText();
+            ArrayList<Topic> topics = (ArrayList) topicManager.topics();
+            argument_TextField.setText("");
+
+            for (Topic topic : topics) {
+
+                if (topic.name.equals(topicToUnsubscribe)) { // TODO Handle exception when I can't find an existing topic?   
+
+                    Subscriber subscriber = new SubscriberImpl(SwingClient.this);
+                    Subscription_check subscription_check = topicManager.unsubscribe(topic, subscriber);
+
+                    if (subscription_check.result == Result.OKAY) {
+                        my_subscriptions.remove(topic);
+                        messages_TextArea.append("Unsuscribed from topic: " + topic.name + "\n");
+                        refreshTopicsSubscriptionsBoxes();
+                    }
+                }
+            }
+        }
     }
 
-    public void windowOpened(WindowEvent e) {
+    class postEventHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+
+            String msg = argument_TextField.getText();
+            argument_TextField.setText("");
+
+            if (publisher != null) {
+                publisher.publish(new Message(publisherTopic, msg));
+            }
+        }
     }
 
-    public void windowClosing(WindowEvent e) {
-      
-      //...
-      
-      System.out.println("one user closed");
-      System.exit(0);
+    class CloseAppHandler implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("all users closed");
+            System.exit(0);
+        }
     }
-  }
-}
+
+    class CloseWindowHandler implements WindowListener {
+
+        public void windowDeactivated(WindowEvent e) {
+        }
+
+        public void windowActivated(WindowEvent e) {
+        }
+
+        public void windowIconified(WindowEvent e) {
+        }
+
+        public void windowDeiconified(WindowEvent e) {
+        }
+
+        public void windowClosed(WindowEvent e) {
+        }
+
+        public void windowOpened(WindowEvent e) {
+        }
+
+        public void windowClosing(WindowEvent e) {
+            // Interface Map.Entry<K,V>
+            // A map entry (key-value pair)
+            for(Entry<Topic, Subscriber> entry : my_subscriptions.entrySet()){
+                Subscriber subscriber = entry.getValue();
+                Topic topic = entry.getKey();
+                subscriber.onClose(new Subscription_close(topic, Cause.SUBSCRIBER));
+            }
+            
+            if(publisher != null){
+                topicManager.removePublisherFromTopic(publisherTopic);
+            }
+            
+            System.out.println("one user closed");
+        }
+    }}
