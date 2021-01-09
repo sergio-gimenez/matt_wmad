b15610ad7e04bb9e3f4b8caf5bc858fb05ba39c4
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-01-09 08:19:36 -0500

    Implementation of WebSocketClient

--- 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_client_exercise/src/webSocketService/WebSocketClient.java
+++ 3_InstantMessaging_exercise/InstantMessagingRemote/InstantMessagingRemote_client_exercise/src/webSocketService/WebSocketClient.java
@@ -15,70 +15,91 @@ import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
 import subscriber.Subscriber;
 import util.Subscription_request;
+import util.Subscription_request.Type;
 
 @ClientEndpoint
 public class WebSocketClient {
 
-  static Map<Topic, Subscriber> subscriberMap;
-  static Session session;
+    static Map<Topic, Subscriber> subscriberMap;
+    static Session session;
 
-  public static void newInstance() {
-    subscriberMap = new HashMap<Topic, Subscriber>();
-    try {
-      WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-      session = container.connectToServer(WebSocketClient.class,
-        URI.create(Cons.SERVER_WEBSOCKET));
-    } catch (Exception e) {
-      e.printStackTrace();
+    public static void newInstance() {
+        subscriberMap = new HashMap<Topic, Subscriber>();
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            session = container.connectToServer(WebSocketClient.class,
+                    URI.create(Cons.SERVER_WEBSOCKET));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
-  
-  public static void close() {
-    try {
-      session.close();
-    } catch (Exception e) {
-      e.printStackTrace();
+
+    public static void close() {
+        try {
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static synchronized void addSubscriber(Topic topic, Subscriber subscriber) {
-    try {
-      
-      //...
-      
-    } catch (Exception e) {
-      e.printStackTrace();
+    public static synchronized void addSubscriber(Topic topic, Subscriber subscriber) {
+        try {
+            if (!subscriberMap.containsKey(topic)) {
+                // Prepare the request
+                Subscription_request subscriptionReq = new Subscription_request(topic, Type.ADD);
+
+                // Encode in JSON and send
+                String request = new Gson().toJson(subscriptionReq);
+                session.getBasicRemote().sendText(request);
+                subscriberMap.put(topic, subscriber);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  public static synchronized void removeSubscriber(Topic topic) {
-    try {
-      
-      //...
-      
-    } catch (Exception e) {
-      e.printStackTrace();
+    public static synchronized void removeSubscriber(Topic topic) {
+        try {
+            if (subscriberMap.containsKey(topic)) {
+                // Prepare the request
+                Subscription_request subscriptionReq = new Subscription_request(topic, Type.REMOVE);
+                // Encode in JSON and send
+                String request = new Gson().toJson(subscriptionReq);
+                session.getBasicRemote().sendText(request);
+                subscriberMap.remove(topic);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  @OnMessage
-  public void onMessage(String json) {
+    @OnMessage
+    public void onMessage(String json) {
 
-    Gson gson = new Gson();
-    Subscription_close subs_close = gson.fromJson(json, Subscription_close.class);
+        Gson gson = new Gson();
+        Subscription_close subscription_close = gson.fromJson(json, Subscription_close.class);
 
-    //ordinary message from topic:
-    if (subs_close.cause==null) {
-      
-      //...
-      
+        Subscriber subscriber;
+        //ordinary message from topic:
+        if (subscription_close.cause == null) {
+            Message msg = new Gson().fromJson(json, Message.class);
+            subscriber = subscriberMap.get(msg.topic);
+
+            if (subscriber != null) {
+                subscriber.onMessage(msg);
+            }
+            
+        } 
+        
+        //ending subscription message:
+        else {
+            subscriber = subscriberMap.get(subscription_close.topic);
+            if(subscriber != null){
+                subscriber.onClose(subscription_close);
+                subscriberMap.remove(subscription_close.topic);
+            }            
+        }
     }
-    //ending subscription message:
-    else {
-      
-      //...
-      
-    } 
-  }
 
 }
